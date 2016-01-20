abe621f3fdd93a95079133aa701d53c3f7b11066
Marcos Mirabent <marcos@mirabent.net>
2016-01-20 11:28:26 -0500

    Added history stuff from Kali and localrc sourcing

---
+++ bash/00_history.bash
@@ -0,0 +1,10 @@
+# don't put duplicate lines or lines starting with space in the history
+HISTCONTROL=ignoreboth
+
+# set the size of the history and the history file
+HISTSIZE=1000
+HISTFILESIZE=2000
+
+# check the window size after each command
+shopt -s checkwinsize
+
